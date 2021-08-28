3ddd5ca83d5dcfb01800f4affb6ea0adbaafb32f
Toberal98 <douglazalberto78@gmail.com>
2021-08-27 19:29:57 -0600

    estilos finales para la app primer Examen

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ const App = () => {
     <>
       <Image
         source={{
-          uri: 'https://www.crushpixel.com/big-static16/preview4/banner-design-coffee-shop-restaurant-2306261.jpg',
+          uri: 'https://lh3.googleusercontent.com/kv6n0foBWAWRxUBupRKFaZ7v0YAsmzEmFr1_SN4p2vW97Us4tn-NYaL74Kh8pOxDEFlbENBnkwa0SXh5W6Al-67DNFPeqcShEly-UZVqnPf9mJ28aPdSqqlHKmHY-ZKoO1PDzqpPk8b9ARLA0I4abOpnAsChvJnGU1yPokQjCUBuSc2mfR3cDiEKM3Cu-IUJWj2AkytQVn3dCrfdK4n8mE4L-LKL0WPkxwSiaXHObFnSGtaGcdizsYIEDCF9AVG4AuOwDl7QzuWHA-dbkNFzk0DM-HV1Ii9NEmx-l2f_bUVrd_eFCUFiTTdxnG70-UzygOq_fzaD2-Gt3m1rg3fvTEUO2RqvWUiyxjkpM5qG76gszfJ0Npi1PlwTFam5t9xpisFJNExL2Z0nyUDvqdo8guygfxppngOqOKH0J7NdCy27NlVOa0HGCl5m37mtkQC5MzUdbIycesNTjOc4-kU0LNB_TA4ruN1psEQMwRqCYOphEMGMo7TCLDgOFr3jRr9FcI_VelcCoLilGkea2tK8q4Qt2M-0qPBHKCi-9HCpb0W7t9vTDf6bSaegYnHfVMATA_lHOGWyXonZ6lrQsLHlI4UL-bdgeM_n2AJldg0=w518-h322-no',
           cache: 'only-if-cached',
         }}
-        style={{width: 400, height: 200}}
+        style={{width: 400, height: 250}}
       />
       <Text style={styles.titleText}>Nuestra Carta</Text>
       <View
@@ -21,42 +21,42 @@ const App = () => {
         <View style={{flex: 0.5, margin: 5}}>
           <Image
             source={{
-              uri: 'https://www.estrategiaynegocios.net/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=ETHiLgSlj2QsooaPSmCVyM$daE2N3K4ZzOUsqbU5sYv8Wnw$QYDSc7uiymdASjoa6FB40xiOfUoExWL3M40tfzssyZqpeG_J0TFo7ZhRaDiHC9oxmioMlYVJD0A$3RbIiibgT65kY_CSDiCiUzvHvODrHApbd6ry6YGl5GGOZrs-&CONTENTTYPE=image/jpeg',
+              uri: 'https://imagenes.elpais.com/resizer/neqM_ZSW1PsUvG47t4tLalTQk-o=/1960x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/W232DWSRSO7AR7DSDH7N7HJA64.jpg',
               cache: 'only-if-cached',
             }}
             style={{width: 150, height: 150, marginBottom: 5}}
           />
           <Text style={styles.texto}>
-            Bebidas Frias
+            Carnes
           </Text>
           <Image
             source={{
-              uri: 'https://www.baque.com/wp-content/uploads/2017/09/Las-mejores-obras-de-arte-de-cafe-en-Instagram.jpg',
+              uri: 'https://s1.eestatic.com/2016/10/11/cocinillas/cocinillas_162247460_116258365_1024x576.jpg',
               cache: 'only-if-cached',
             }}
             style={{width: 150, height: 150, marginBottom: 5}}
           />
           <Text style={styles.texto}>
-            Entradas
+            Ensaladas
           </Text>
         </View>
         <View style={{flex: 0.5, margin: 5}}>
           <Image
             source={{
-              uri: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2018/05/300919-cafe-mas-cafeina.jpg?itok=x6vKeIT-',
+              uri: 'https://www.cocinayvino.com/wp-content/uploads/2020/06/Mariscos-1068x713.jpg',
               cache: 'only-if-cached',
             }}
             style={{width: 150, height: 150, marginBottom: 5}}
           />
-          <Text style={styles.texto}>Postres</Text>
+          <Text style={styles.texto}>Mariscos</Text>
           <Image
             source={{
-              uri: 'https://www.discovertravelnews.com/wp-content/uploads/2018/11/cafes-1300x731.jpg',
+              uri: 'https://s1.eestatic.com/2019/04/04/ciencia/nutricion/bebidas_espirituosas-alcohol-energia_388473428_119607645_1706x1280.jpg',
               cache: 'only-if-cached',
             }}
             style={{width: 150, height: 150, marginBottom:5}}
           />
-          <Text style={styles.texto}>Cafes</Text>
+          <Text style={styles.texto}>Bebidas</Text>
         </View>
       </View>
       <View>
@@ -75,24 +75,28 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 10,
+    color:'#008f39'
   },
   texto:{
     fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 10
+    marginBottom: 10,
+    color: '#FFFF00'
   },
   alumnos:{
     marginTop:350,
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
+    color:'#ff6600'
   },
   alumnos1:{
     marginTop:0,
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
+    color: '#ff6600'
   }
 });
 
